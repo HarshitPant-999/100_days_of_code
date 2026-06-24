42b86fc37e6d7012de87dfeda7fccc75291d43f1
Bananabob-hp3 <pantharahit97@gmail.com>
2026-06-24 19:33:33 +0530

    Adding decorator exercise which is creating my own decorator function to measure the amount of seconds that a function takes to execute

---
+++ decorator_exercise/decorator_exercise.py
@@ -0,0 +1,34 @@
+import time
+current_time = time.time()
+print(current_time) # seconds since Jan 1st, 1970 
+
+# Write your code below 👇
+def speed_calc_decorator(function):
+    def wrapper_function():
+        start = time.perf_counter()
+        result = function()
+        end = time.perf_counter()
+        diff = end - start
+        print(f"Time is: {diff:.6f} seconds")
+        return diff
+    return wrapper_function
+
+@speed_calc_decorator
+def fast_function():
+  for i in range(1000000):
+    i * i
+
+@speed_calc_decorator
+def slow_function():
+  for i in range(10000000):
+    i * i
+
+fast_time = fast_function()
+slow_time = slow_function()
+diff = slow_time - fast_time
+ratio = slow_time/fast_time 
+print(diff)
+print(ratio)
+
+
+
