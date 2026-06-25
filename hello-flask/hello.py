6e6ae2ee5abb24fcc8b93cc66d28650282ffc439
Bananabob-hp3 <pantharahit97@gmail.com>
2026-06-25 15:26:38 +0530

    Practice of day 55 working Flask URL paths and the Flask Debugger

--- hello-flask/hello.py
+++ hello-flask/hello.py
@@ -5,12 +5,16 @@ print(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
-
+    return "Hello, World!"
 
 @app.route("/bye")
 def say_bye():
-     return "Bye"
+     return "Bye!"
+
+@app.route("/username/<name>/<int:number>")
+def greet(name, number):
+    return f"Hello there {name}, you are {number} years old!"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
+ 
