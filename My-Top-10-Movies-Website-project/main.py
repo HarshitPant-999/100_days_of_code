38ae3ded1420747643f7a9bce093dd0668884f3d
Bananabob-hp3 <pantharahit97@gmail.com>
2026-07-08 23:00:11 +0530

    Working on Movies-Website-project: 30% progress made

--- My-Top-10-Movies-Website-project/main.py
+++ My-Top-10-Movies-Website-project/main.py
@@ -6,35 +6,72 @@ from sqlalchemy import Integer, String, Float
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
-import requests
+import csv
 
-'''
-Red underlines? Install the required packages first: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from requirements.txt for this project.
-'''
+db = SQLAlchemy()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap5(app)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///movie_list.db"
+db.init_app(app)
 
-# CREATE DB
+class MovieForm(FlaskForm):
+    title = StringField("title")
+    year = StringField("year")
+    description = StringField("Description")
+    rating = StringField("rating")
+    ranking = StringField("ranking")
+    review = StringField("review")
+    img_url = StringField("img_url")
+    submit = StringField("submit")
 
 
-# CREATE TABLE
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=False, nullable=False)
+    year = db.Column(db.String(250), unique=False, nullable=False)
+    description = db.Column(db.String(2500), nullable=False)
+    rating = db.Column(db.Float(10), nullable=False)
+    ranking = db.Column(db.Float(250), nullable=False)
+    review = db.Column(db.String(10), nullable=False)
+    img_url = db.Column(db.String(10), nullable=False)
 
+
+with app.app_context():
+    db.create_all()
+    new_movie = Movie(
+        title="Phone Booth",
+        year=2002,
+        description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
+        rating=7.3,
+        ranking=10,
+        review="My favourite character was the caller.",
+        img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
+    )
+    db.session.add(new_movie)
+    db.session.commit()
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    all_movies = db.session.execute(db.select(Movie)).scalars().all()
+    return render_template("index.html", movies=all_movies)
 
+def add():
+    form = MovieForm()
+    if form.validate_on_submit():
+        new_movie = Movie(
+            title=form.title.data,
+            year=form.year.data,
+            description=form.description.data,
+            rating=form.rating.data,
+            ranking=form.ranking.data,
+            review=form.review.data,
+            img_url=form.img_url.data
+        )
+        db.session.add(new_movie)
+        db.session.commit()
+    render_template("add.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
